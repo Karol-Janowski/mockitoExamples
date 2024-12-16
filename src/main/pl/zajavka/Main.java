adeeefcbd4c61e9d618b68393cbeec847bb63570
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-12-16 13:37:41 +0100

    added some notes about testing

--- src/main/pl/zajavka/Main.java
+++ src/main/pl/zajavka/Main.java
@@ -2,6 +2,22 @@ package pl.zajavka;
 
 public class Main {
     public static void main(String[] args) {
-
+        // TDD - Test Driven Development
+        // rodzaje testow:
+        // - jednostkowe - szybkie i najtansze w utrzymaniu - bledny implementacyjne w danej dziedzinie programu
+        // -integracyjne - przetestowanie komponentow miedzy ktorymi dochodzi do interakcji, test integracyjny jest jezeli
+        // testuje wiecej niz jedna klase - styk dwoch komponentow - klasa z klasa/paczka z paczka - wolniejsze i wieksze
+        // przykladowo kod komunikuje sie ze sztuczna baza danych stworzona na potrzeby testow
+        // -systemowe/end-to-end - testy na prawdziwej aplikacji maja na celu zasymulowanie prawdziwego uzytkownika,
+        // zazwyczaj sa zastepowane przez testerow manualnych
+        // selenium testing - technologia do testow e2e
+        // piramida testow - zalety i wady poszczegolnych rodzajow testow
+        // ISTQB - Miedzynarodowa Rada certyfikacji do testowania oprogramowania
+        // International Software Testing Qualifications Board
+        // SUT - System Under Test - okreslenie tego co jest  testowane
+        // CUT - class under test
+        // OUT - object under test
+        // MUT - Method under test
+        // AUT - application under test
     }
 }
